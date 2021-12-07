11d5d4988101178eac5221ab9a198aca9bf0dcf5
tellebma <66943979+tellebma@users.noreply.github.com>
2021-12-07 09:24:41 +0100

    Update main-receiver.py

--- microbits/main-receiver.py
+++ microbits/main-receiver.py
@@ -1,6 +1,9 @@
 #//MAXIME
 from microbit import *
 import radio
+from ProtocoleRadio import *
+
+IDENTIFIANT_RADIO = 1
 
 def getFromTel()->None:
     """
@@ -24,95 +27,18 @@ def ask(fonction:str)->None:
     fonction:
         TL or LT
     """
-    ProtocoleRadio().send_message("2",fonction)
+    ProtocoleRadio(IDENTIFIANT_RADIO).send_message("2",fonction)
     #Demande latemperature a l'autre microbit
     tentative = 0
     while True:
         tentative += 1
-        source,message = ProtocoleRadio().get_message()
+        source,message = ProtocoleRadio(IDENTIFIANT_RADIO).get_message()
         if source and message:
             if source == "2": 
                 return message
         if tentative > 200:
-            ProtocoleRadio().send_message("2",fonction)
+            ProtocoleRadio(IDENTIFIANT_RADIO).send_message("2",fonction)
             tentative = 0
-
-class Secu:
-    """
-    Class de sécurité, comprend toutes les fonctionnalités liées à la Sécu entre les microbits.       
-    Securité. vraiment très sur!
-    Algo de césar remodifié car on est vraiement très chaud  ! 
-    """
-    def crypt(self,message:str,key:int=10)->str:
-        """
-        Passe un message clair, le return en message crypté 
-        Utilisation d'un césar modifié.
-        """
-        cryptedMessage = ""
-        for lettre in message:
-            key+=1 #pour chaque itération on décale la key de 1 
-            cryptedMessage += chr(ord(lettre)+key)
-        return cryptedMessage
-
-    def decrypt(self,cryptedMessage:str,key:int=10)->str:
-        """
-        Passe un message crypté et une clef, le return en message clair. 
-        Utilisation d'un césar modifié.
-        """
-        clearMessage = ""
-        for lettre in cryptedMessage:
-            key+=1 #pour chaque itération on décale la key de 1 
-            clearMessage += chr(ord(lettre)-key)
-        return clearMessage
-        
-    def checksum(self,message:str)->str:
-        """
-        fonction de checksum entre les deux microbits.
-        Nous permet de verifier l'intégrité du message.
-        #Reste très basic car si un octet du message est incrémenté et un second décrémenté alors l'intégrité sera vue comme juste.
-        """
-        checksum = 0
-        for letter in message:
-            checksum += ord(letter)
-        return str(checksum)
-    
-    """
-    import hashlib ne fonctionne pas :/
-    def checksum(self,message):
-        return hashlib.md5(message.encode()).hexdigest()
-    """
-    
-    
-class ProtocoleRadio:
-    """
-    Classe protocoleRadio.
-	Comprend toutes les fonctionnalités pour envoyer et recevoir des messages 
-    """
-    def __init__(self)->None:
-	self.moi = "1"#Mon identifiant sur le protocole.
-    
-    def get_message(self)->str and str:
-        """
-        Fonctionnalité pour recevoir les messages sur le protocole qui me sont destinés.
-        """
-        recep = radio.receive()
-        if recep:
-            message_recup = Secu().decrypt(recep).split("|")
-            if message_recup[0] == self.moi:
-                if Secu().checksum(message_recup[0]+"|"+message_recup[1]+"|"+message_recup[2]) == message_recup[3]:
-                    #le message s'addresse a moi.
-                    return message_recup[1],message_recup[2]#1 source,message
-        return 0,0
-
-    def send_message(self,dest,msg):
-        """
-        Envoie un message crypté sous le format "dest|source|message|checksum"
-        """
-        to_send_informations = dest+"|"+self.moi+"|"+msg
-        to_send = to_send_informations+"|"+Secu().hash(to_send_informations)        
-        radio.send(Secu().crypt(to_send))
-        #return 1 pour send.
-        return 1
 
 
 """CODE PRINCIPAL POUR LE RECEIVER CONNECTE A LA PASSERELLE"""
